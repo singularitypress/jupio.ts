456b37cf915db70c45b4fd65783f39e310698153
Jay Pandya <jaypandya2k5@gmail.com>
2022-02-16 15:22:24 -0500

    create next app with tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React, { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return <div className="container">Hello</div>;
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "theme-base": "#121212",
+        "theme-dp01": "#1D1D1D",
+        "theme-dp02": "#212121",
+        "theme-dp03": "#242424",
+        "theme-dp04": "#272727",
+        "theme-dp06": "#2c2c2c",
+        "theme-dp08": "#2d2d2d",
+        "theme-dp12": "#323232",
+        "theme-dp16": "#353535",
+        "theme-dp24": "#373737",
+        "theme-accent": "#faf9f5",
+      },
+    },
+  },
+  plugins: [],
+};
