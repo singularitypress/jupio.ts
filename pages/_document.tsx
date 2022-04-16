c6a3044793d44882e56f45582a8f30219befd5be
Jay Pandya <jaypandya2k5@gmail.com>
2022-04-16 13:57:41 -0400

    added fontawesome icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default () => {
   return (
     <Html>
+      <Head />
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,6 @@ import Head from "next/head";
 
 const Home: NextPage = () => {
   const headingText = "Jupio Labs";
-  const bodyText =
-    "Unlocking the potential of our clients, open source, and our staff.";
 
   const img = {
     light: "bg-[url('https://i.imgur.com/csNmS00.png')]",
@@ -24,12 +22,18 @@ const Home: NextPage = () => {
         <title>Home</title>
       </Head>
       <VideoHero bg={vid.light} darkBg={vid.dark}>
-        <div className="z-30 w-screen h-screen flex flex-col justify-center">
+        <div className="z-30 h-screen flex flex-col justify-center">
           <Header variant="h1" className="text-9xl">
             {headingText}
           </Header>
           <hr className="h-0 border-t-4 border-t-theme-base dark:border-t-theme-accent w-1/2 mt-8" />
-          <p className="mb-8 mt-8 text-2xl">{bodyText}</p>
+          <p className="mb-8 mt-8 text-2xl">
+            Don't have time to get it together and need everything designed and
+            built right away?
+            <br />
+            <br />
+            <strong>You're in the right place.</strong>
+          </p>
           <Button element="link" href="/work">
             Our Work
           </Button>
